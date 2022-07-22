48cd2b2fb2447b6d8b564fbe8290faeb5f2d24e1
mostofa kamal <mostofakamal1990@gmail.com>
2022-07-22 10:15:26 +0600

    state and action created

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
 
 
 // State 
-  /*first we should initial State assign in a variable , 
-  it could be  object  assign then under variables 
+  /* 
+   first we should initial State assign in a variable , 
+  it could be  object  assign then under variables ,
+  state means data  what data i would  process or worked
   example 
 
   */
@@ -28,4 +30,40 @@
  * action means what  am i want to do in my application or my app
  * like INCREMENT COUNTER OR DECREMENT COUNTER
  * 
+ * ============= action is a object =============
+ *  Two things in action 
+ *   1. Type 
+ *        example  {type: 'INCREMENT'}
+ *   2. payload 
+ *     example {payload: 10} 
+ *  but we use a function  then  inside function  object return 
+ * 
+ * 
+ */
+// Defined constant type declaration
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+const ADD_USER = "ADD_USER";
+
+const incrementCounterAction = () =>{
+  return   {
+        type:INCREMENT
+    }
+}
+
+const decrementCounterAction = ()=>{
+    return{
+        type:DECREMENT
+    }
+}
+  
+const addUserAction = () =>{
+    return{
+        type:ADD_USER,
+        payload:{name:'kamal'}
+    }
+} 
+
+/**
+ * payload used for data sent in reducer
  */
