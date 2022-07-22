5c677e7c7bd76131c5b9741e739942c5c89e157c
mostofa kamal <mostofakamal1990@gmail.com>
2022-07-22 15:52:45 +0600

    multiple reducrs

--- product.js
+++ product.js
@@ -1,5 +1,5 @@
 
-const {createStore} = require('redux');
+const {createStore, combineReducers} = require('redux');
 // step 1: state
 
 const initializeProductState ={
@@ -33,7 +33,9 @@ const productReducer = (state = initializeProductState, action)=>{
             case GET_PRODUCTS:
                 return{
                     ...state,
-                    products: state.products
+                   // ...state,
+                    //products: state.products,
+                    
                 }
 
             case ADD_PRODUCT:
@@ -42,18 +44,85 @@ const productReducer = (state = initializeProductState, action)=>{
                     products:[...state.products, action.payload]
                 }
             default:
-                break;
+              return  state;
         }
 }
 
 // step 4: store
 
-const store = createStore(productReducer);
+// const store = createStore(productReducer);
+
+// store.subscribe(()=>{
+//     console.log(store.getState())
+// })
+
+// store.dispatch(setProduct('potato'))
+// store.dispatch(setProduct('lemon'));
+// store.dispatch(getProduct())
+
+
+
+// cart 
+
+// step 1: state
+
+const initalizeCartItem ={
+    products:[],
+    count:0
+}
+
+
+// step 2: action 
+
+const GET_CART_ITEM = 'GET_CART_ITEM';
+const SET_CART_ITEM = 'SET_CART_ITEM';
+const getCartItem = ()=>{
+    return{
+        type:GET_CART_ITEM
+    }
+}
+
+const setCartItem = (product)=>{
+    return{
+        type:SET_CART_ITEM,
+        payload:product
+    }
+}
+
+
+const cartReducer = (state = initalizeCartItem, action) =>{
+
+    switch(action.type){
+
+        case GET_CART_ITEM:
+            return{
+                state
+            }
+
+        case SET_CART_ITEM:
+            return{
+              //  ...state,
+                products:[...state.products, action.payload],
+                count : state.count + 1
+            }
+        default:
+            return state
+    }
+}
+
+// Multiple Reducer 
+const rootReducer = combineReducers({
+    productRe: productReducer,
+    cartItemRe: cartReducer
+
+})
+// const store = createStore(cartReducer);
+const store = createStore(rootReducer);
 
 store.subscribe(()=>{
     console.log(store.getState())
 })
-
-store.dispatch(setProduct('potato'))
-store.dispatch(setProduct('lemon'));
-store.dispatch(getProduct())
+// store.dispatch(productReducer('potato'))
+store.dispatch(setCartItem('potato'))
+store.dispatch(setCartItem('lemon'));
+store.dispatch(getCartItem())
