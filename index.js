48c9792eb620f3d12820261fa48f57acd3def346
mostofa kamal <mostofakamal1990@gmail.com>
2022-07-22 09:50:26 +0600

    redux basic install

---
+++ index.js
@@ -0,0 +1,31 @@
+// step 1: state defined
+// step 2: action 
+
+
+// State 
+  /*first we should initial State assign in a variable , 
+  it could be  object  assign then under variables 
+  example 
+
+  */
+    const initialCounterState = {
+        count:0,
+        // users:[
+        //     {name:'kamal'}
+        // ]
+    }
+
+    const initialUsersState = {
+         users:[
+                {name:'kamal'}
+            ]
+    }
+
+
+// action 
+/**
+ * If i click button then something happen  
+ * action means what  am i want to do in my application or my app
+ * like INCREMENT COUNTER OR DECREMENT COUNTER
+ * 
+ */
